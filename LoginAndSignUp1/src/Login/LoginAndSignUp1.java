65c53e8351f829a92eca07de79cf5c6c1ffea9d1
Shadow21345 <147471692+Shadow21345@users.noreply.github.com>
2023-10-11 22:29:36 -0700

    Add files via upload

---
+++ LoginAndSignUp1/src/Login/LoginAndSignUp1.java
@@ -0,0 +1,16 @@
+
+package Login;
+
+public class LoginAndSignUp1 {
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+    
+}
